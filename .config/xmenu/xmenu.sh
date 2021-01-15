6c05d761ebdff2ade41c9c1a164c1809306795dc
Simen Dager Sneve <smndagersneve@gmail.com>
2021-01-15 15:05:55 +0100

    Added xmenu configuration

---
+++ .config/xmenu/xmenu.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# SET NOEXPANDTAB!!!!
+
+cat <<EOF | xmenu | sh &
+Communication
+	Web browser	firefox
+	Discord	discord
+	Slack	slack
+Productivity
+	Office	onlyoffice-desktopeditors
+	PDF Viewer	evince
+	Emacs	emacs
+	Calibre	calibre
+	Anki	anki
+	GNU Octave	octave --gui
+	Gramps	gramps
+Multimedia
+	Spotify	spotify
+	Pocket Casts	/home/simen/Programs/Appimages/pocket-casts-linux-1.2.1-x86_64.AppImage
+	Vlc	vlc
+	Audacity	audacity
+	Darktable	darktable
+	Krita	krita
+Games
+	Steam	steam
+	Lutris	lutris
+	Itch.io	itch
+	Minecraft	multimc
+	Godot Engine	godot
+Utilities
+	Files	nemo
+	Terminal	st
+	Torrent	qbittorrent
+
+
+Lock	/home/simen/Programs/Scripts/lock.sh
+Shutdown		poweroff
+Reboot			reboot
+EOF
