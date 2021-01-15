664b6fda413b1f26bcda606376baff0ea161ca54
Simen Dager Sneve <smndagersneve@gmail.com>
2021-01-15 17:24:18 +0100

    Added some comments and changed screenshot list order

--- Programs/Scripts/screenshot.sh
+++ Programs/Scripts/screenshot.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 # Small script to take screenshot with dmenu and scrot
 
-case "$(printf "A selected window\\nCurrent window\\nBoth monitors\\nOnly the first monitor" | dmenu -l 6 -p "Screenshot which area?")" in
-	"A selected window") scrot -sf -e 'mv $f ~/Multimedia/Pictures/Screenshots/' ;;
+case "$(printf "Current window\\nA selected window\\nBoth monitors\\nOnly the first monitor" | dmenu -l 6 -p "Screenshot which area?")" in
 	"Current window") scrot -u -e 'mv $f ~/Multimedia/Pictures/Screenshots/' ;;
+	"A selected window") scrot -sf -e 'mv $f ~/Multimedia/Pictures/Screenshots/' ;;
 	"Both monitors") scrot -m -e 'mv $f ~/Multimedia/Pictures/Screenshots/' ;;
     "Only the first monitor") scrot -a $(xrandr | grep '\Wconnected\W' | grep -Po "\d+x\d+\+\d+\+\d+" | sed 's/x/+/' | awk -F "+" '{print $3","$4","$1","$2}' | head -n 1 | tail -n 1) -e 'mv $f ~/Multimedia/Pictures/Screenshots/' ;;
 esac
--- Programs/Scripts/updpkgls.sh
+++ Programs/Scripts/updpkgls.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+# A script to make a list of all explicitly installed packages to be used with cron
 
 # Arch:
 pacman -Qet | awk '{print $1}' >> ~/.local/share/package-list
