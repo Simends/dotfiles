b5e4dcfb10e047cec3bf0bf1e1bb9452eb041998
Simen Dager Sneve <smndagersneve@gmail.com>
2021-01-26 18:10:48 +0100

    Added a new script and did som minor configurations

--- .local/bin/updpkgls.sh
+++ .local/bin/updpkgls.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 # A script to make a list of all explicitly installed packages to be used with cron
 
+rm -f ~/.local/share/package-list
+
 # Arch:
 pacman -Qet | awk '{print $1}' >> ~/.local/share/package-list
