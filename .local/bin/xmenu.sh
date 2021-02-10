f65eb58d80c5479a6baebde90e5ea4e90733a564
Simen Dager Sneve <smndagersneve@gmail.com>
2021-02-10 16:43:53 +0100

    Various changes

--- .local/bin/xmenu.sh
+++ .local/bin/xmenu.sh
@@ -10,45 +10,47 @@
 # SET NOEXPANDTAB!!!!
 
 cat <<EOF | xmenu | sh &
-Communication
-	Web browser	firefox
-	Qutebrowser	qutebrowser
+
+
+ Communication
+	IMG:/usr/share/icons/hicolor/48x48/apps/firefox.png	Web browser	firefox
+	IMG:/usr/share/icons/hicolor/48x48/apps/qutebrowser.png	Qutebrowser	qutebrowser
 	Discord	discord
 	Slack	slack
 	Microsoft Teams	teams
 	Zoom	zoom
-Productivity
-	Office	onlyoffice-desktopeditors
-	PDF Viewer	evince
-	Emacs	emacsclient -c
-	Calibre	calibre
+ Productivity
+	IMG:/usr/share/icons/hicolor/48x48/apps/onlyoffice-desktopeditors.png	Office	onlyoffice-desktopeditors
+	IMG:/usr/share/icons/hicolor/48x48/apps/emacs.png	Emacs	emacsclient -c
+	IMG:/usr/share/icons/hicolor/48x48/apps/calibre-gui.png	Calibre	calibre
 	Anki	anki
-	GNU Octave	octave --gui
-	Gramps	gramps
-Multimedia
-	Spotify	spotify
-	Pocket Casts	/home/simen/.local/bin/Appimages/pocket-casts-linux-1.2.1-x86_64.AppImage
-	Vlc	vlc
-	Stremio	stremio
-	Darktable	darktable
-	Krita	krita
-Games
-	Steam	steam
-	Lutris	lutris
-	Itch.io	itch
+	IMG:/usr/share/icons/hicolor/48x48/apps/octave.png	GNU Octave	octave --gui
+ Multimedia
+	IMG:/usr/share/icons/hicolor/48x48/apps/spotify.png	Spotify	spotify
+	IMG:/home/simen/.local/share/icons/hicolor/48x48/apps/appimagekit-pocket-casts-linux.png	Pocket Casts	/home/simen/.local/bin/Appimages/pocket-casts.AppImage
+	IMG:/usr/share/icons/hicolor/48x48/apps/vlc.png	Vlc	vlc
+	IMG:/usr/share/icons/hicolor/48x48/apps/darktable.png	Darktable	darktable
+	IMG:/usr/share/icons/hicolor/48x48/apps/krita.png	Krita	krita
+ Games
+	IMG:/usr/share/icons/hicolor/48x48/apps/steam.png	Steam	steam
+	IMG:/usr/share/icons/hicolor/48x48/apps/lutris.png	Lutris	lutris
+	IMG:/usr/share/icons/hicolor/48x48/apps/itch.png	Itch.io	itch
 	Minecraft	multimc
 	Godot Engine	godot
-Utilities
+ Utilities
 	Files	nemo
 	Terminal	kitty
-	Torrent	qbittorrent
-	Virtual Machine	virt-manager
-	Timeshift	timeshift-launcher
-	Wine	playonlinux
+	IMG:/usr/share/icons/hicolor/48x48/apps/qbittorrent.png	Torrent	qbittorrent
+	IMG:/usr/share/icons/hicolor/48x48/apps/virt-manager.png	Virtual Machine	virt-manager
+	IMG:/usr/share/icons/hicolor/48x48/apps/timeshift.png	Timeshift	timeshift-launcher
+	CoreCtrl	corectrl
+
 
 
 Lock	/home/simen/.local/bin/lock.sh
 Log out	killall xinit
 Shutdown		loginctl poweroff
 Reboot			loginctl reboot
+
+
 EOF
