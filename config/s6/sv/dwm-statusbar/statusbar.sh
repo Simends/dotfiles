18c343947d16ebaa27dd265e3b4c0fbddbb16837
Simen Dager Sneve <smndagersneve@gmail.com>
2022-03-05 18:48:31 +0100

    Changes to s6 service scripts

--- config/s6/sv/dwm-statusbar/statusbar.sh
+++ config/s6/sv/dwm-statusbar/statusbar.sh
@@ -16,8 +16,60 @@ while true; do
 
     # CPU Temp
     tmpicon=" "
-	tmpval=$(cat /sys/class/thermal/thermal_zone2/temp | sed 's|$| / 1000|' | bc)
+	tmpval=$(cat /sys/class/thermal/thermal_zone10/temp | sed 's|$| / 1000|' | bc)
     tmp="${tmpicon}${tmpval}°C${seperator}"
+
+    # Net
+    netstat=$(connmanctl state | grep -oP '(?<=State = )\w*$')
+    if [ "$netstat" == "online" ] || [ "$netstat" == "ready" ]; then
+        neticon=""
+    else
+        neticon=""
+    fi
+    net="${neticon}${seperator}"
+
+    # Battery
+    batdir="/sys/class/power_supply/BAT0"
+    if [ -d "$batdir" ]; then
+        baticon=" "
+        batstaus=$(cat "$batdir/status")
+        if [ "$batstaus" == "Charging" ]; then
+            baticon=" "
+        fi
+        batcap=$(cat "$batdir/capacity")
+        bat="${baticon}${batcap}%${seperator}"
+    else
+        bat=""
+    fi
+
+    # Backlight
+    if [ -f /usr/bin/light ]; then
+        balicon=" "
+        balstat=$(light -G | sed 's/\..*$/%/')
+        bal="${balicon}${balstat}${seperator}"
+    else
+        bal=""
+    fi
+
+    # Audio
+    audstat=$(awk -F"[][]" '/Left:/ { print $4 }' <(amixer sget Master))
+    if [ "${audstat}" == "off" ]; then
+        audicon=""
+        aud="${audicon}${seperator}"
+    else
+        audicon=" "
+        audlvl=$(awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master))
+        aud="${audicon}${audlvl}${seperator}"
+    fi
+
+    # Mic
+    micstat=$(awk -F"[][]" '/Left:/ { print $4 }' <(amixer sget Capture))
+    if [ "${micstat}" == "off" ]; then
+        micicon=""
+        mic="${micicon}${seperator}"
+    else
+        mic=""
+    fi
 
     # Do not disturb
     dndicon=""
@@ -38,12 +90,6 @@ while true; do
         mpr=""
     fi
 
-    # TODO: Audio
-
-    # TODO: Battery
-
-    # TODO: Net
-
-	xsetroot -name " ${dnd}${mpr}${tmp}${mem}${dte} "
+	xsetroot -name " ${mpr}${dnd}${mic}${aud}${bal}${bat}${net}${tmp}${mem}${dte} "
 	sleep "${delay}"
 done
